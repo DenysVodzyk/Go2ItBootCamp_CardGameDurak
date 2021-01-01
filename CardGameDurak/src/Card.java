bb81b0f4c14d02603b470a039b62c2e162591b25
DenysVodzyk <vodzyk@gmail.com>
2020-12-31 19:18:57 -0500

    Class Card

---
+++ CardGameDurak/src/Card.java
@@ -0,0 +1,43 @@
+import java.util.Objects;
+
+public class Card {
+    private CARD_RANK card_rank;
+    private SUIT suit;
+
+
+    public Card(CARD_RANK card_rank, SUIT suit) {
+        this.card_rank = card_rank;
+        this.suit = suit;
+    }
+
+    public SUIT getSuit() {
+        return suit;
+    }
+
+    public CARD_RANK getCard_rank() {
+        return card_rank;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return card_rank == card.card_rank &&
+                suit == card.suit;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(card_rank, suit);
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "card_rank=" + card_rank +
+                ", suit=" + suit +
+                '}' + "\n";
+    }
+}
+
