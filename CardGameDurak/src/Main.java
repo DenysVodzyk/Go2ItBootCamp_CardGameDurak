c4618dbca7776bedbae4c3d14fc8360eff0a7a6a
DenysVodzyk <vodzyk@gmail.com>
2020-12-31 19:22:06 -0500

    Main class

---
+++ CardGameDurak/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Deck deck = new Deck(36);
+
+        System.out.println(deck.getDeck());
+
+        System.out.println("");
+        System.out.println(deck.getTrumpCardSuit());
+//        System.out.println(SUIT.values().length);
+//        System.out.println(SUIT.values()[0]);
+//        System.out.println(SUIT.values()[1]);
+//        System.out.println(SUIT.values()[2]);
+//        System.out.println(SUIT.values()[3]);
+//
+//        //       ACE(9), KING(8), QUEEN(7), JACK(6), TEN(5), NINE(4), EIGHT(3), SEVEN(2), SIX(1);
+//
+//        System.out.println("");
+//        System.out.println(CARD_RANK.values().length);
+//        System.out.println(CARD_RANK.values()[0]);
+//        System.out.println(CARD_RANK.values()[8].getRank());
+//
+//        Card card = new Card(CARD_RANK.values()[0], SUIT.values()[2]);
+//        System.out.println(card);
+    }
+}
