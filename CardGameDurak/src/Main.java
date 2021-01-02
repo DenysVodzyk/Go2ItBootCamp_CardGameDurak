4f77fdf522d0fcb68b4d534974e25f220f1407ad
DenysVodzyk <vodzyk@gmail.com>
2021-01-01 20:31:44 -0500

    Moved most of the methods to separate class

--- CardGameDurak/src/Main.java
+++ CardGameDurak/src/Main.java
@@ -1,12 +1,67 @@
 public class Main {
     public static void main(String[] args) {
+        GameService gameService = new GameService(4);
+        gameService.startGame();
 
-        Deck deck = new Deck(36);
 
+
+
+
+
+        /*Deck deck = new Deck();
+        GameService gameService = new GameService(4);
+        deck.setTrumpCardSuit();
+        System.out.println("Trump suit: " + deck.getTrumpCardSuit());
+        System.out.println("Deck before cards distribution: ");
+        System.out.println(deck.getDeckSize());
         System.out.println(deck.getDeck());
 
-        System.out.println("");
-        System.out.println(deck.getTrumpCardSuit());
+        System.out.println("-------------------");
+        System.out.println("Player 1 hand: ");
+        Player player1 = new Player(1);
+        gameService.dealFirstSixCardToPlayer(player1);
+        System.out.println(player1.getHand());
+
+        System.out.println("-------------------");
+        System.out.println("Player 2 hand: ");
+        Player player2 = new Player(2);
+        gameService.dealFirstSixCardToPlayer(player2);
+        System.out.println(player2.getHand());
+
+        System.out.println("-------------------");
+        System.out.println("Player 3 hand: ");
+        Player player3 = new Player(3);
+        gameService.dealFirstSixCardToPlayer(player3);
+        System.out.println(player3.getHand());
+
+        System.out.println("-------------------");
+        System.out.println("Player 4 hand: ");
+        Player player4 = new Player(4);
+        gameService.dealFirstSixCardToPlayer(player4);
+        System.out.println(player4.getHand());
+
+        System.out.println("-------------------");
+
+        System.out.println("Deck after cards distribution: ");
+        System.out.println(deck.getDeckSize());
+        System.out.println(deck.getDeck());
+
+        System.out.println("Lowest Trump card on hand: " + gameService.getLowestTrumpCardOnHand());
+*/
+
+
+
+        /*System.out.println(deck.getDeck());
+        Card card1 = deck.getDeck().iterator().next();
+        System.out.println("First card in deck is: " + card1);
+        deck.getDeck().remove(card1);
+        System.out.println(deck.getDeck());
+
+
+        System.out.println(deck.getDeckSize());
+        */
+
+
 //        System.out.println(SUIT.values().length);
 //        System.out.println(SUIT.values()[0]);
 //        System.out.println(SUIT.values()[1]);
@@ -18,7 +73,8 @@ public class Main {
 //        System.out.println("");
 //        System.out.println(CARD_RANK.values().length);
 //        System.out.println(CARD_RANK.values()[0]);
-//        System.out.println(CARD_RANK.values()[8].getRank());
+//        System.out.println(CARD_RANK.values()[8]);
+//        System.out.println(CARD_RANK.valueOf("SIX").ordinal());
 //
 //        Card card = new Card(CARD_RANK.values()[0], SUIT.values()[2]);
 //        System.out.println(card);
