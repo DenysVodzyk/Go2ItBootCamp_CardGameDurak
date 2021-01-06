46044a915776ffd3abc14050dc5274266d58b8e9
DenysVodzyk <vodzyk@gmail.com>
2021-01-05 20:28:05 -0500

    Method name refactor

--- CardGameDurak/src/CardRank.java
+++ CardGameDurak/src/CardRank.java
@@ -1,9 +1,9 @@
-public enum CARD_RANK {
+public enum CardRank {
     ACE(9), KING(8), QUEEN(7), JACK(6), TEN(5), NINE(4), EIGHT(3), SEVEN(2), SIX(1);
 
     private final int rank;
 
-    private CARD_RANK(int rank) {
+    private CardRank(int rank) {
         this.rank = rank;
     }
 
